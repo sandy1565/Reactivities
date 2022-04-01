c5fce5f2372fda9c65b8bcbddc93bd52aa0bdb41
sandeepkumar <sandeepkumar@univ-wea.com>
2022-04-01 15:24:32 +0530

    added clinet app

---
+++ client-app/src/App.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
+
+import logo from './logo.svg';
+import './App.css';
+
+import { Header, List } from 'semantic-ui-react';
+
+function App() {
+
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      setActivities(response.data);
+    })
+  }, []);
+
+  return (
+    <div>
+      <Header as="h2" icon={'users'} content="Reactivities"/>
+      <List>
+        {
+          activities.map((item: any) => (
+            <List.Item key={item.id}>
+              {item.title}
+            </List.Item>
+          ))
+        }
+        </List>
+    </div>
+  );
+}
+
+export default App;
